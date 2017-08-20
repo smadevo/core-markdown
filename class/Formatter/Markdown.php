a7b82bc2cca8f50a6021e1a3ad7fdfe9e84ed2a9
Jens Scherbl <jens@scherbl.com>
2017-08-20 23:18:33 +0200

    Refactored Markdown formatter

--- class/Formatter/Markdown.php
+++ class/Formatter/Markdown.php
@@ -5,18 +5,40 @@ use App\Formatter;
 use League\CommonMark;
 
 /**
+ * Converts Markdown to HTML.
+ *
  * @inheritDoc
  */
-final class Markdown extends CommonMark\CommonMarkConverter implements Formatter
+final class Markdown implements Formatter
 {
     /**
-     * @inheritDoc
+     * @var CommonMark\Converter
      */
-    public function __construct()
+    private $converter;
+
+    /**
+     * Bootstraps an instance of itself.
+     *
+     * @return self
+     *
+     * @uses CommonMark\CommonMarkConverter
+     * @uses CommonMark\Environment
+     */
+    public static function create(): self
     {
-        parent::__construct([
+        return new self(new CommonMark\CommonMarkConverter([
             'html_input' => CommonMark\Environment::HTML_INPUT_STRIP
-        ]);
+        ]));
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param CommonMark\Converter $converter
+     */
+    public function __construct(CommonMark\Converter $converter)
+    {
+        $this->converter = $converter;
     }
 
     /**
@@ -24,6 +46,6 @@ final class Markdown extends CommonMark\CommonMarkConverter implements Formatter
      */
     public function format(string $input): string
     {
-        return $this->convertToHtml($input);
+        return $this->converter->convertToHtml($input);
     }
 }
